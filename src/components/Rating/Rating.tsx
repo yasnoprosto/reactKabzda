0f5d2b2069e2419859063c8b85e88e7da74b781c
Denis Pochinskiy <ircxenon1@gmail.com>
2023-06-08 12:38:13 +0500

    uncontrolled components

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -9,14 +9,19 @@ export const Rating = (props: RatingPropsType) => {
     console.log("Rating is rendering");
 
     const [selectedStar, setSelectedStar] = useState<number>(0);
+    const setStarHandler = (value: number) => {
+      setSelectedStar(value)
+    }
+
+
 
     return (
         <div>
-            <Star selected={selectedStar > 0} /><button onClick={() => {setSelectedStar(1)}}>1</button>
-            <Star selected={selectedStar > 1} /><button onClick={() => {setSelectedStar(2)}}>2</button>
-            <Star selected={selectedStar > 2} /><button onClick={() => {setSelectedStar(3)}}>3</button>
-            <Star selected={selectedStar > 3} /><button onClick={() => {setSelectedStar(4)}}>4</button>
-            <Star selected={selectedStar > 4} /><button onClick={() => {setSelectedStar(5)}}>5</button>
+            <Star selected={selectedStar > 0} callback={setStarHandler} value={1}/>
+            <Star selected={selectedStar > 1} callback={setStarHandler} value={2}/>
+            <Star selected={selectedStar > 2} callback={setStarHandler} value={3}/>
+            <Star selected={selectedStar > 3} callback={setStarHandler} value={4}/>
+            <Star selected={selectedStar > 4} callback={setStarHandler} value={5}/>
         </div>
     );
 };
